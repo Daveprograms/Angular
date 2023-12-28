5cdba480fa0005c42d9a40f3e0794d3824224c3a
David Fashola <davidprograms7@gmsil.com>
2023-12-27 23:20:59 -0500

    push

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // creating an array of object
+
+  salesPersonList: SalesPerson[]= [
+    new SalesPerson("David", "Fashola", "Davfash1@gmail.com",5000 ),
+    new SalesPerson("Emmanuel", "Fash", "Emmanuel1@gmail.com", 10000),
+    new SalesPerson("Rachel", "Obu", "Rachel1@gmail.com",9000 ),
+    new SalesPerson("Temi", "Uby", "Temi1@gmail.com", 3000)
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
